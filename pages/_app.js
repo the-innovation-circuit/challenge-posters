ce3c7d6872100fc4b7d209e79ae3f10761d92386
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-16 15:52:53 +1100

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import NextApp from 'next/app'
+import '@the-innovation-circuit/theme/fonts/fonts.css'
+import theme from '@the-innovation-circuit/theme'
+import { ThemeProvider } from 'theme-ui'
+import ColorSwitcher from '../components/color-switcher'
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <ColorSwitcher />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
+}
